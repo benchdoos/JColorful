d5079066edc862e8922664ffe0e64334694cd6df
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2019-02-01 23:05:40 +0300

    Code cleanup

--- src/main/java/com/github/benchdoos/serializers/BinaryElementDeserializer.java
+++ src/main/java/com/github/benchdoos/serializers/BinaryElementDeserializer.java
@@ -7,15 +7,9 @@ import com.google.gson.*;
 import java.awt.*;
 import java.lang.reflect.Type;
 
-
-import com.google.gson.*;
-
-import java.lang.reflect.Type;
-
 public class BinaryElementDeserializer implements JsonDeserializer<BinaryElement>{
     public BinaryElement deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
         JsonObject object = jsonElement.getAsJsonObject();
-        System.out.println("obj: " + object);
 
         final JsonElement objectType = object.get("objectType");
         if (objectType.getAsString().equalsIgnoreCase("common")) {
