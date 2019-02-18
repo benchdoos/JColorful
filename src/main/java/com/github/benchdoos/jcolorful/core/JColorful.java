42ca3c22e1a7d093eed771eaf7c32d24c77d20eb
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2019-02-18 19:42:48 +0300

    Added Atomizer as interface

--- src/main/java/com/github/benchdoos/jcolorful/core/JColorful.java
+++ src/main/java/com/github/benchdoos/jcolorful/core/JColorful.java
@@ -17,7 +17,7 @@ public class JColorful {
     //todo add annotations (what components to ignore)
     public static final Theme EXTREMELY_BLACK = getTheme("/presets/extremelyBlack.json");
     public static final Theme DARK_GRAY = getTheme("/presets/darkGray.json"); //for next releases
-    private Theme theme = null;
+    private Atomizer atomizer;
 
     /**
      * Class  {@code JColorful} is the main class of the JColorful library.
@@ -29,7 +29,6 @@ public class JColorful {
         log.info("JColorful initializing...");
     }
 
-
     /**
      * Class  {@code JColorful} is the main class of the JColorful library.
      *
@@ -40,9 +39,16 @@ public class JColorful {
      */
     public JColorful(Theme theme) {
         log.info("JColorful initializing...");
-        if (theme == null) throw new IllegalArgumentException("Theme bean can not be null");
-        this.theme = theme;
+        if (theme == null) throw new IllegalArgumentException("Theme can not be null");
+        atomizer = new DefaultAtomizer(theme);
         log.info("JColorful was set a Theme: {}", theme);
+    }
+
+    public JColorful(Atomizer atomizer) {
+        log.info("JColorful initializing...");
+        if (atomizer == null) throw new IllegalArgumentException("Atomizer can not be null");
+        this.atomizer = atomizer;
+        log.info("JColorful was set a Theme: {}", atomizer.getTheme());
     }
 
     /**
@@ -60,7 +66,6 @@ public class JColorful {
         return new ThemeImpl(content);
     }
 
-
     /**
      * Colorizes {@link Component} by settings, that got in {@link Theme}.
      * Method colorize <u>all</u> childes of the component recursively.
@@ -69,20 +74,18 @@ public class JColorful {
      */
 
     public void colorize(Component component) {
-        if (theme == null) throw new RuntimeException("Theme should be set.");
-
         if (component instanceof Container) {
             Container container = (Container) component;
             final Component[] components = container.getComponents();
             for (final Component coloringComponent : components) {
                 if (coloringComponent != null) {
-                    new Atomizer(theme).colorize(coloringComponent);
+                    atomizer.colorize(coloringComponent);
                     colorize(coloringComponent);
                 }
             }
         } else {
             if (component != null) {
-                new Atomizer(theme).colorize(component);
+                atomizer.colorize(component);
             }
         }
     }
@@ -93,7 +96,7 @@ public class JColorful {
      * <b>ATTENTION!</b> Applies to whole project (all components).
      */
     public void colorizeGlobal() {
-        new Atomizer(theme).colorizeGlobal();
+        atomizer.colorizeGlobal();
     }
 
     /**
@@ -104,9 +107,25 @@ public class JColorful {
      * @throws RuntimeException if {@link Theme }is not set
      */
     public void colorizeSingle(Component component) {
-        if (theme == null) throw new RuntimeException("Theme should be set.");
+        atomizer.colorize(component);
+    }
 
-        new Atomizer(theme).colorize(component);
+    /**
+     * Gives back {@link Atomizer} that was set
+     *
+     * @return curr {@link Atomizer}
+     */
+    public Atomizer getAtomizer() {
+        return atomizer;
+    }
+
+    /**
+     * Sets {@link Atomizer}, that colorizes components
+     *
+     * @param atomizer to colorize
+     */
+    public void setAtomizer(Atomizer atomizer) {
+        this.atomizer = atomizer;
     }
 
     /**
@@ -115,20 +134,8 @@ public class JColorful {
      * @return current {@link Theme}
      */
     public Theme getTheme() {
-        return theme;
-    }
-
-    /**
-     * Setter for {@link Theme}
-     *
-     * @param theme to set for {@code JColorful}
-     * @return {@link JColorful} itself
-     * @throws IllegalArgumentException if {@link Theme} is not set.
-     */
-    public JColorful setTheme(Theme theme) {
-        if (theme == null) throw new IllegalArgumentException("Theme can not be null");
-        this.theme = theme;
-
-        return this;
+        if (atomizer != null) {
+            return atomizer.getTheme();
+        } else return null;
     }
 }
